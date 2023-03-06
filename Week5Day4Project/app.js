d13917186802ce2aef29d14e0ddee410b2826446
DavidJimenez5 <DAVID.JIMENEZ1899@STU.MTEC.EDU>
2023-03-05 18:48:59 -0700

    Created first jquery project

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,7 @@
+$("#nav").children().each(function() {
+    $(this).css("color", "white");
+})
+$("#nav").css("background-color", "#145");
+$("li:even").css("background-color", "#ccc");
+$("li:odd").css("background-color", "#aaa");
+$("#myInput").val("David");
